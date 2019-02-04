1dcacd64a32dd4186211a3ec6e162eefec043cf0
Zeyu Li <zeyu.li@RBE.sk.ca>
2019-02-04 12:04:49 -0600

    V1.3 init

--- loops.py
+++ loops.py
@@ -3,11 +3,7 @@
 def main():
     number1 = 9
     array = [1,2,3,4]
-    loop(array)
-
-def loop(array):
-    for number2 in array:
-        print(number2)
+    print(array.append(1))
 
 if __name__ == "__main__":
     main()
--- print.py
+++ print.py
@@ -3,7 +3,9 @@
 
 def main():
     number1 = 9
-    print(f"Hellow, number is {number1}")
+    print(f"Hellow, number is {number1}\n")
+    print("Hellow, number is %d", number1)
+    print()
     
 if __name__ == "__main__":
     main()
