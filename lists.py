a9dbce592f97d3eb5f47e612cba7155480e6ea44
Zeyu Li <zeyu.li@RBE.sk.ca>
2019-02-05 12:00:19 -0600

    Lists V1.0

---
+++ lists.py
@@ -0,0 +1,125 @@
+# Organizing List of British Colonies
+
+def main():
+    foo = """Aden Protectorate
+Anglo-Egyptian Sudan
+Auckland Islands
+Bahamas
+Bahrain
+Balleland
+Bangladesh
+Barbados
+Basutoland
+Bechuanaland
+Bermuda
+British Borneo
+British Cameroons
+British East Africa
+British Egypt
+British Egypt
+British Guiana
+British Honduras
+British India
+British Malaya
+British Solomon Islands
+British Somaliland
+British Togoland
+British Western Pacific Territories
+Brunei
+Burma
+Ceylon
+Colonial Fiji
+Colonial Nigeria
+Colony of Newfoundland
+Corsica
+Cyprus
+Emirate of Transjordan
+Falkalnd Islands
+Federated Malay States
+Gambia Colony and Protectorate
+Gilbert and Ellice Islands
+Gold Coast
+Heligoland
+Hong Kong
+India
+Indian Reserve
+Ionian Islands
+Ireland
+Island of St. John
+Jamaica
+Kingdom of Rarotonga
+Kingdom of Sarawak
+Leeward Islands
+Malta
+Mandatory Iraq
+Mandatory Palestine
+Menorca
+Mosquito Coast
+Muscat and Oman
+Nauru
+New Hebrides
+New Hebrides
+New South Wales
+Niue
+North Borneo
+Northern Rhodesia
+Nyasaland
+Oregon Country
+Pakistan
+Phoenix Islands
+Province of East Florida
+Province of Nova Scotia
+Province of Quebec
+Province of West Florida
+Qatar
+Queensland
+Rupert's Land
+Sheikhdom of Kuwait
+Sierra Leone Colony and Protectorate
+Singapore
+Solomon Islands
+South Africa
+South Arabia
+South Australia
+South Georgia
+South-West Africa
+Southern Rhodesia
+Straits Settlements
+Sultanate of Zanzibar
+Swan River Colony
+Swaziland
+Tanganyika Territory
+Territory of New Guinea
+Territory of Papua
+Territory of Papua New Guinea
+Thirteen Colonies
+Tokelau
+Tonga
+Trucial States
+Uganda Protectorate
+Unfederated Malay States
+Van Diemen's Land
+Victoria
+Western Samoa
+Windward Islands
+"""
+
+    array = foo.splitlines()
+    print(f"There use to be {len(array)} colonies that belong to the UK")
+
+    # array.reverse()
+    # print(array)
+
+    popped_array = array.pop()
+    print(f"The last British colony in terms of alphabetical order is {popped_array}")
+
+    # for foo in revered_colonies:
+    #     print(foo, end=',\n', flush=True)
+
+    print(array[0])
+
+    # del array[0]
+    
+
+if __name__ == "__main__":
+    main()
