ee693d58b8c749505a595778a33437e86a9e5d42
Zeyu Li <zeyu.li@RBE.sk.ca>
2019-02-12 12:03:23 -0600

    Orginize into folders

--- Working/rand.py
+++ Working/rand.py
@@ -9,13 +9,9 @@ By: Andrew Li
 
 """
 
-import random
 
 def main():
-    x=0
-    while x < 1200:
-        print(random.randint(0,1), end=" ")
-        x +=1
+    
 
 if __name__ == "__main__":
     main()
