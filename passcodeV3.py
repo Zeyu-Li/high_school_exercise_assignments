7ada1fbe1d4d5169bee947dc9b53f5b587d60891
Andrew432 <zeyu.li@rbe.sk.ca>
2019-03-03 21:39:46 -0600

    Passcode V3 Init

---
+++ passcodeV3.py
@@ -0,0 +1,143 @@
+""" 
+Passcode program, by Andrew Li 
+Version 3.0 Working
+This program will be a simple user-password program
+"""
+
+# all modules needed for secure databases
+import bleach                       # cleans inputs
+import json                         # writes to json | Might switch to SQL
+from argon2 import PasswordHasher   # hashes passwords
+import os                           # reads from system files
+
+# logins to account
+def login(user, foo):
+
+    # input passcode
+    passcode = bleach.clean(input('Password: '))
+
+    # checks passcode
+
+    try:
+        data = json.load()
+
+    #if fails, try again
+    except:
+
+        print('Password does not match the username, try again')
+        return 1
+
+# creates user
+def createUser(user, foo):
+
+    newPasscode = bleach.clean(input('New password: '))
+
+    if (len(newPasscode) < 8):
+        print('Length of password is too short. \nThe passwork has to be at least 8 characters long')
+        return 1
+
+    retypedPasscode = bleach.clean(input('Retype password: '))
+
+    if (newPasscode == retypedPasscode):
+
+        #insert some hash and salt function with argon2
+
+        hashedPassword = PasswordHasher().hash(retypedPasscode)
+
+        with open('password.json', 'r',) as fp:
+
+            feeds = json.load(fp)
+
+        with open('password.json', 'w', 'utf-8') as feedsjson:
+            values = [{user: hashedPassword}]
+            feeds.append(values)
+            print(json.dump(feeds, feedsjson))
+
+            # close json file
+            open('password.json', 'w').close
+
+        print('Account created')
+        return 0
+
+    else:
+
+        print('Passwords do not match. Try again.')
+        return 1
+
+# gets username
+def username():
+
+    lengthCondition = True
+
+    # input and clean username
+    while lengthCondition:
+        name = bleach.clean(input('Username: '))
+
+        if (len(name) >= 5):
+            lengthCondition = False
+        else:
+            print('Username must be at least 5 characters long\n')
+
+    return [checkUsername(name), name]
+
+###### File checks #######
+
+# checks username
+def checkUsername(username):
+
+    # does json file exist
+    isFile()
+
+    # does user exist
+    return checkUser(username)
+
+# check if username exists
+def checkUser(username):
+
+    with open('password.json', 'r') as fp:
+        try: 
+            json.load(username in fp)
+            return True
+        except:
+            return False
+
+# checks if file exists
+def isFile():
+
+    # creates json if none exists
+    if (os.path.isfile('password.json') == False):
+
+        # create json file
+        with open('password.json', 'w') as passcode1:
+            exists = True
+            json.dump(exists, passcode1)
+
+# main calls entire system
+def main():
+
+    print('This is The Passcode Program \n Please enter your username')
+
+    namedUser = username()
+
+    if (namedUser[0] == False):
+
+        print('Welcome new user!\n')
+
+        foo = True
+
+        while foo:
+            foo = createUser(namedUser[1], foo)
+
+    else: 
+
+        foo = True
+
+        while foo:
+            foo = login(namedUser[1], foo)
+
+    print("Done operations!")
+
+
+# system calls name which triggers main
+if __name__ == "__main__":
+    main()
