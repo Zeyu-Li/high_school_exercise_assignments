dcc640aeda39281541f8b80a6eb997e97db7cce5
Andrew <zeyu.li23@gmail.com>
2019-03-05 20:30:20 -0600

    Password Program working and surce (maybe)

--- passcodeV3.py
+++ passcodeV3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """ 
 Passcode program, by Andrew Li 
 Version 3.0 Working
@@ -17,162 +16,23 @@ def login(user, foo):
     # input passcode
     passcode = bleach.clean(input('Password: '))
 
-    # checks passcode
-
-    try:
-        data = json.load()
-
-    #if fails, try again
-    except:
-
-        print('Password does not match the username, try again')
-        return 1
-
-# creates user
-def createUser(user, foo):
-
-    newPasscode = bleach.clean(input('New password: '))
-
-    if (len(newPasscode) < 8):
-        print('Length of password is too short. \nThe passwork has to be at least 8 characters long')
-        return 1
-
-    retypedPasscode = bleach.clean(input('Retype password: '))
-
-    if (newPasscode == retypedPasscode):
-
-        #insert some hash and salt function with argon2
-
-        hashedPassword = PasswordHasher().hash(retypedPasscode)
-
-        pyData = { user: hashedPassword }
-
-        # dumps password into json file
-        with open('passwords.json', 'w') as fp:
-
-            json.dump(pyData, fp)
-
-        print('Account created')
-        return 0
-
-    else:
-
-        print('Passwords do not match. Try again.')
-        return 1
-
-# gets username
-def username():
-
-    lengthCondition = True
-
-    # input and clean username
-    while lengthCondition:
-        name = bleach.clean(input('Username: '))
-
-        if (len(name) >= 5):
-            lengthCondition = False
-        else:
-            print('Username must be at least 5 characters long\n')
-
-    return [checkUsername(name), name]
-
-###### File checks #######
-
-# checks username
-def checkUsername(username):
-
-    # does json file exist
-    isFile()
-
-    # does user exist
-    return checkUser(username)
-
-# check if username exists
-def checkUser(username):
-
+    # checks passcode, else retry password
     with open('passwords.json', 'r') as fp:
-        try: 
-            json.load(username in fp)
-            return True
-        except:
-            return False
+        data = json.load(fp)
 
-# checks if file exists
-def isFile():
-
-    # creates json if none exists
-    if (os.path.isfile('passwords.json') == False):
-
-        # create json file
-        with open('passwords.json', 'w') as passcode1:
-            exists = True
-            json.dump(exists, passcode1)
-
-# main calls entire system
-def main():
-
-    print('This is The Passcode Program \n Please enter your username')
-
-    namedUser = username()
-
-    if (namedUser[0] == False):
-
-        print('Welcome new user!\n')
-
-        foo = True
-
-        while foo:
-            foo = createUser(namedUser[1], foo)
-
-    else: 
-
-        foo = True
-
-        while foo:
-            foo = login(namedUser[1], foo)
-
-    print("Done operations!")
-
-
-# system calls name which triggers main
-if __name__ == "__main__":
-    main()
-=======
-""" 
-Passcode program, by Andrew Li 
-Version 3.0 Working
-This program will be a simple user-password program
-"""
-
-# all modules needed for secure databases
-import bleach                       # cleans inputs
-import json                         # writes to json | Might switch to SQL
-from argon2 import PasswordHasher   # hashes passwords
-import os                           # reads from system files
-
-# logins to account
-def login(user, foo):
-
-    # input passcode
-    passcode = bleach.clean(input('Password: '))
-
-    # checks passcode
-
-    try:
-        data = json.loads()
-
-    # if fails, try again
-    except:
-
-        print('Password does not match the username, try again')
-        return 1
+        if (PasswordHasher().verify(data.get(user), str(passcode)) == True ):
+            print('Successfully logged in')
+            return 0
+        else:
+            print('Password does not match the username, try again\n')
+            return 1
 
 # creates user
 def createUser(user, foo):
 
     newPasscode = bleach.clean(input('New password: '))
 
-    if (len(newPasscode) < 1): # temp change
+    if (len(newPasscode) < 8): # temp change
         print('Length of password is too short. \nThe passwork has to be at least 8 characters long')
         return 1
 
@@ -236,8 +96,8 @@ def checkUser(username):
 
     with open('passwords.json', 'r') as fp:
         try: 
-            json.load(username in fp)
-            return True
+            check = json.load(fp)
+            return (username in check)
         except:
             return False
 
@@ -254,38 +114,27 @@ def isFile():
 # main calls entire system
 def main():
 
-    # print('This is The Passcode Program \n Please enter your username')
+    print('This is The Passcode Program \n Please enter your username')
 
-    with open('passwords.json', 'r') as fp:
-    
-        try: 
-            thing = json.load("sadder" in fp)
-            print('is in thing')
-            print(thing)
-
-        except:
-            print('is not in it')
-            print(json.load("sadder" in fp))
-
-    # namedUser = username()
+    namedUser = username()
 
     # # redo this
 
-    # if (namedUser[0] == False):
+    if (namedUser[0] == False):
 
-    #     print('Welcome new user!\n')
+        print('Welcome new user!\n')
 
-    #     foo = True
+        foo = True
 
-    #     while foo:
-    #         foo = createUser(namedUser[1], foo)
+        while foo:
+            foo = createUser(namedUser[1], foo)
 
-    # else: 
+    else: 
 
-    #     foo = True
+        foo = True
 
-    #     while foo:
-    #         foo = login(namedUser[1], foo)
+        while foo:
+            foo = login(namedUser[1], foo)
 
     print("Done operations!")
 
@@ -293,4 +142,3 @@ def main():
 # system calls name which triggers main
 if __name__ == "__main__":
     main()
->>>>>>> 2881e9fd7e5018aa623add6b3a5d32a63f065e1e
